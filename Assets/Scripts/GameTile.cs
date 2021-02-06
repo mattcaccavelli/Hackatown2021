7443a22d6bc46add2f39feedc0ffcf314cc16ded
mattcaccavelli <matt.caccavelli@gmail.com>
2021-02-06 17:30:10 -0500

    added grass

--- Assets/Scripts/GameTile.cs
+++ Assets/Scripts/GameTile.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class GameTile
 {
     public Vector2 position;
-    public TileType tiletype;
+    public TileType tiletype = TileType.Grass;
 
     public float timer = 0;
 
     public int stage = 0;
     int stepTime = 0;
+
+    public GameTile(int _x, int _y)
+    {
+        position.x = _x;
+        position.y = _y;
+    }
 
     public void Plant()
     {
@@ -22,6 +28,10 @@ public class GameTile
 
     }
 
+    public void Till()
+    {
+
+    }
 
 
 }
